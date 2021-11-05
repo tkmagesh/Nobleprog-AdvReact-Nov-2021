db6fedbef2505c429b98c8a062618431f5303c45
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-05 10:57:31 +0530

    data file for server communication

--- bug-tracker-app/src/store/index.js
+++ bug-tracker-app/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import bugsReducer from '../bugs/reducers/bugsReducer';
 import projectsReducer from '../projects/reducers/projectsReducer'
 
@@ -7,9 +7,19 @@ const rootReducer = combineReducers({
     projectState : projectsReducer
 });
 
-//const store = createStore(bugsReducer);
-//const store = createStore(projectsReducer);
+function loggerMiddleware(store){
+    return function(next){
+        return function(action){
+            console.group(action.type);
+            console.log('%c state', 'color: gray', store.getState());
+            console.log(action);
+            next(action);
+            console.log('%c state', 'color: gray', store.getState());
+            console.groupEnd();
+        }
+    }
+}
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 export default store;
